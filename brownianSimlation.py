a184c9bc7ed0d6cab41f0b286e84caec8d3e1bb9
hexaquarks <seseriusca@gmail.com>
2022-09-16 20:48:44 -0400

    Tweak the plots a little

--- brownianSimlation.py
+++ brownianSimlation.py
@@ -1,4 +1,6 @@
 from typing import List, Tuple
+from matplotlib.pyplot import figure
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes,mark_inset
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -18,10 +20,6 @@ class Util:
         return path[0][-1], path[1][-1]
 
 class Brownian:
-    """
-    Constructor, note that just like in C++ we can do default 
-    initialization in function/construtor parameter
-    """
     def __init__(self, x0 = 0):
         assert (type(x0) == float or type(x0) == int or x0 is None), "Expect a float or None for the initial value"
         
@@ -33,10 +31,6 @@ class Brownian:
         return (self._x, self._y)
     
     def generateRandomWalks(self, steps:int = 100) -> PATH:
-        """
-        steps is the number of data points we consider between each 
-        random walk, it's the standard step size, higher for higher precision
-        """
         w = np.ones(steps) * self.x0 # array of size steps of els 0.
         
         for i in range(1, steps):
@@ -47,9 +41,6 @@ class Brownian:
         return w
     
     def generateNormal(self, steps = 100) -> PATH:
-        """
-        Generate motion by drawing from the Normal distribution
-        """
         wx = np.ones(steps) * self.x0
         wy = np.ones(steps) * self.x0
         
@@ -69,18 +60,37 @@ paths: List[PATH] = [Brownian().get_path for i in range(NUMBER_OF_PATHS)]
 colors: List = ['r', 'b', "orange", 'g', 'y', 'c']
 markers: List = ['o', 'v', '<', '>', 's', 'p']
 
+fig, ax = plt.subplots(figsize=[8, 6])
+
 for i, path in enumerate(paths):
-    plt.plot(path[0], path[1], c = colors[i])
+    ax.plot(path[0], path[1], c = colors[i])
     
     last_point = Util.get_last_point(path)
-    print("x,y : " ,last_point[0] , " ", last_point[1])
-    plt.plot(last_point[0], last_point[1], 
+    ax.plot(last_point[0], last_point[1], 
              marker = markers[i], markersize = 12,
              markerfacecolor = colors[i])
     
 x_min, x_max, y_min, y_max = Util.get_bounds(paths)
 MAX = max(abs(el) for el in [x_min, x_max, y_min, y_max])
 MAX *= 1.1
-plt.xlim(-MAX, MAX)
-plt.ylim(-MAX, MAX)
+ax.set_xlim(-MAX, MAX)
+ax.set_ylim(-MAX, MAX)
+
+## ticks 
+ax.tick_params(axis='y',
+               direction="in",
+               right=True, labelsize=18)
+ax.tick_params(axis='x', direction="in" , top=True,bottom=True, labelsize=18)
+
+## legends and utilities
+ax.set_xlabel(r"x", fontsize=16)
+ax.set_ylabel(r"y", fontsize=16)
+
+## border colors
+ax.patch.set_edgecolor('black')  
+ax.patch.set_linewidth('2') 
+
+
 plt.show(block=True)
+
+fig.tight_layout()
