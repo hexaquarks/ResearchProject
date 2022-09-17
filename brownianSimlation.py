6022994e3464b630e2218c126f30523d2d556c85
hexaquarks <seseriusca@gmail.com>
2022-09-16 20:40:27 -0400

    Add utility functions, and end point markers

--- brownianSimlation.py
+++ brownianSimlation.py
@@ -2,6 +2,21 @@ from typing import List, Tuple
 import numpy as np
 import matplotlib.pyplot as plt
 
+PATH = Tuple[List[float]]
+
+class Util:
+    @staticmethod
+    def get_bounds(lists: List[PATH]) -> Tuple[int]:
+        X_MAX = max([max(elem[0]) for elem in lists])
+        Y_MAX = max([max(elem[1]) for elem in lists])
+        X_MIN = min([min(elem[0]) for elem in lists])
+        Y_MIN = min([min(elem[1]) for elem in lists])
+        return X_MIN, X_MAX, Y_MIN, Y_MAX
+    
+    @staticmethod
+    def get_last_point(path: PATH) -> Tuple[int]:
+        return path[0][-1], path[1][-1]
+
 class Brownian:
     """
     Constructor, note that just like in C++ we can do default 
@@ -10,11 +25,14 @@ class Brownian:
     def __init__(self, x0 = 0):
         assert (type(x0) == float or type(x0) == int or x0 is None), "Expect a float or None for the initial value"
         
-        self.x0 = float(x0)  # this.x = x;
-        self.path = []
-        
+        self.x0 = float(x0) 
+        self._x, self._y = self.generateNormal()
     
-    def generateRandomWalks(self, steps:int = 100):
+    @property
+    def get_path(self) -> PATH:
+        return (self._x, self._y)
+    
+    def generateRandomWalks(self, steps:int = 100) -> PATH:
         """
         steps is the number of data points we consider between each 
         random walk, it's the standard step size, higher for higher precision
@@ -28,36 +46,41 @@ class Brownian:
             w[i] = w[i - 1] + (yi / np.sqrt(steps))
         return w
     
-    def generateNormal(self, steps = 100):
+    def generateNormal(self, steps = 100) -> PATH:
         """
         Generate motion by drawing from the Normal distribution
         """
-        w = np.ones(steps) * self.x0
+        wx = np.ones(steps) * self.x0
+        wy = np.ones(steps) * self.x0
         
         for i in range(1, steps):
             # Sampling from the Normal distribution
+            xi = np.random.normal()
             yi = np.random.normal()
             # Weiner process
-            w[i] = w[i - 1] + (yi / np.sqrt(steps))
-            self.path = w
+            wx[i] = wx[i - 1] + (xi / np.sqrt(steps))
+            wy[i] = wy[i - 1] + (yi / np.sqrt(steps))
+        return wx, wy
 
 STEPS: int = 1000
-NUMBER_OF_PATHS: int = 7
-paths: List[Tuple[Brownian]] = []
-colors: List = ['r', 'b', "orange", 'g', 'y', 'c', 'm']
+NUMBER_OF_PATHS: int = 6
 
-for i in range(NUMBER_OF_PATHS):
-    paths.append((Brownian(), Brownian()))
-    paths[i][0].generateNormal(STEPS)
-    paths[i][1].generateNormal(STEPS)
+paths: List[PATH] = [Brownian().get_path for i in range(NUMBER_OF_PATHS)]
+colors: List = ['r', 'b', "orange", 'g', 'y', 'c']
+markers: List = ['o', 'v', '<', '>', 's', 'p']
+
+for i, path in enumerate(paths):
+    plt.plot(path[0], path[1], c = colors[i])
     
-for i in range(NUMBER_OF_PATHS):
-    item: Tuple[Brownian] = paths[i]
-    plt.plot(item[0].path, item[1].path, c = colors[i])
+    last_point = Util.get_last_point(path)
+    print("x,y : " ,last_point[0] , " ", last_point[1])
+    plt.plot(last_point[0], last_point[1], 
+             marker = markers[i], markersize = 12,
+             markerfacecolor = colors[i])
     
-# xmax,xmin,ymax,ymin = x.max(),x.min(),y.max(),y.min()
-# scale_factor = 1.25
-# xmax,xmin,ymax,ymin = xmax*scale_factor,xmin*scale_factor,ymax*scale_factor,ymin*scale_factor
-plt.xlim(-2,2)
-plt.ylim(-2,2)
+x_min, x_max, y_min, y_max = Util.get_bounds(paths)
+MAX = max(abs(el) for el in [x_min, x_max, y_min, y_max])
+MAX *= 1.1
+plt.xlim(-MAX, MAX)
+plt.ylim(-MAX, MAX)
 plt.show(block=True)
