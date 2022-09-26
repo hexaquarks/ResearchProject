ba09377fd478bdad0def58527bc9347ece46e2f2
Nicolas Almerge <44474247+NicolasAlmerge@users.noreply.github.com>
2022-09-26 16:54:50 -0400

    Updated variable case following Python conventions

--- util.py
+++ util.py
@@ -5,28 +5,28 @@ import random
 class Util:
     @staticmethod
     def get_bounds(lists) -> Tuple[int]:
-        X_MAX = max([max(elem[0]) for elem in lists])
-        Y_MAX = max([max(elem[1]) for elem in lists])
-        X_MIN = min([min(elem[0]) for elem in lists])
-        Y_MIN = min([min(elem[1]) for elem in lists])
-        return X_MIN, X_MAX, Y_MIN, Y_MAX
-    
+        x_min = min([min(elem[0]) for elem in lists])
+        x_max = max([max(elem[0]) for elem in lists])
+        y_min = min([min(elem[1]) for elem in lists])
+        y_max = max([max(elem[1]) for elem in lists])
+        return x_min, x_max, y_min, y_max
+
     @staticmethod
     def compute_distance(p1: Tuple, p2: Tuple) -> float:
         return np.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
-        
+
     @staticmethod
     def get_last_point(path) -> Tuple[int]:
         return path[-1][0], path[-1][1]
-    
+
     @staticmethod
     def get_x_coordinates(path) -> List:
         return list(list(zip(*path))[0])
-    
+
     @staticmethod
     def get_y_coordinates(path) -> List:
         return list(list(zip(*path))[1])
-    
+
     @staticmethod
     def get_random_normal_direction():
         return np.random.normal() * np.random.choice([1, -1])
