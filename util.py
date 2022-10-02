ee4f9366d67aab148b9954d3351b517e3353c5e9
hexaquarks <seseriusca@gmail.com>
2022-10-02 16:09:19 -0400

    refactor for util file

--- util.py
+++ util.py
@@ -1,50 +1,44 @@
 import numpy as np
-import random
 
-class Util:
+class Util:  # convert this to an import of a module with global functions
     @staticmethod
-    def get_bounds(lists) -> tuple[int, ...]:
-        x_min: int = min([min(elem[0]) for elem in lists])
-        x_max: int = max([max(elem[0]) for elem in lists])
-        y_min: int = min([min(elem[1]) for elem in lists])
-        y_max: int = max([max(elem[1]) for elem in lists])
-        return x_min, x_max, y_min, y_max
-
-    @staticmethod
-    def compute_distance(p1: tuple, p2: tuple) -> float:
+    def compute_distance(p1: tuple[float, float], p2: tuple[float, float]) -> float:
         return np.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
 
     @staticmethod
-    def get_last_point(path: list[tuple]) -> tuple[int, ...]:
+    def get_last_point(path: list[tuple[float, float]]) -> tuple[float, float]:
         return path[-1][0], path[-1][1]
 
     @staticmethod
-    def get_x_coordinates(path) -> list:
+    def get_x_coordinates(path: list[tuple[float, float]]) -> list[float]:
         return list(list(zip(*path))[0])
 
     @staticmethod
-    def get_y_coordinates(path) -> list:
+    def get_y_coordinates(path: list[tuple[float, float]]) -> list[float]:
         return list(list(zip(*path))[1])
 
     @staticmethod
-    def get_random_normal_direction():
+    def get_random_normal_direction() -> float:
         return np.random.normal() * np.random.choice([1, -1])
 
     @staticmethod
-    def is_point_within_bounds(pos: tuple, bounds: tuple[tuple, ...]):
+    def is_point_within_bounds(
+        pos: tuple[float, float],
+        bounds: tuple[tuple[float, float], tuple[float, float]],
+    ) -> bool:
         x, y = pos[0], pos[1]
-        return x >= bounds[0][0] and x <= bounds[0][1] and y >=  bounds[1][0] and y <= bounds[1][1]
-    
+        return (
+            bounds[0][0] <= x <= bounds[0][1] and
+            bounds[1][0] <= y <= bounds[1][1]
+        )
+
     @staticmethod
-    def sign(x):
-        return ((x >= 0) << 1) - 1
-    
+    def increment_tuple_by_val(tuple_object: tuple[float, float], val: tuple[float, float]) -> tuple[float, float]:
+        return tuple_object[0] + val[0], tuple_object[1] + val[1]
+
     @staticmethod
-    def increment_tuple_by_val(tuple_object: tuple, val):
-        tuple_object = tuple((tuple_object[0] + val[0], tuple_object[1] + val[1]))
-        return tuple_object
-    
-    @staticmethod
-    def change_direction(tuple_object: tuple, dir):
-        tuple_object = tuple((tuple_object[0] - dir[0], tuple_object[1] - dir[1]))
-        return tuple_object
+    def change_direction(
+        tuple_object: tuple[float, float],
+        direction: tuple[float, float],
+    ) -> tuple[float, float]:
+        return tuple_object[0] - direction[0], tuple_object[1] - direction[1]
