e1a371596cd645bbdabbf814f3587022e513f6d5
Nicolas Almerge <44474247+NicolasAlmerge@users.noreply.github.com>
2022-10-07 12:46:01 -0400

    Reformatted

--- simulations/brownian.py
+++ simulations/brownian.py
@@ -1,17 +1,19 @@
 from simulations.simulation import *
-from util import *
+import util
+
 
 class Brownian(Simulation):
     def __init__(self, n: int = 5):
         super().__init__(n)
-        
+
     def update_path(self, idx: int):
         x_dir, y_dir = [
-            Util.get_random_normal_direction() * MEMBRANE_DIFFUSION_FACTOR_CORRECTED for _ in range(2)]
+            util.get_random_normal_direction() * MEMBRANE_DIFFUSION_FACTOR_CORRECTED
+            for _ in range(2)
+        ]
         x, y = self.paths[idx][-1]
         self.paths[idx].append((x + x_dir, y + y_dir))
-        
+
     def update(self) -> None:
         for i in range(self.n_particles):
             self.update_path(i)
-
